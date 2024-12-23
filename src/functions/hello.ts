9988d4a8c288af96e5b8f4c5f14df48840bab37d
João Vitor <joaovitorfelixcontato@gmail.com>
2024-12-23 16:25:32 -0300

    testing serverless framework

---
+++ src/functions/hello.ts
@@ -0,0 +1,10 @@
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Andrézão falando nada com nada 🔥🔥🔥",
+    }),
+  };
+}
